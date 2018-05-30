0d586e9869ba2471e499ae89fc6523e63357ac3a
Zhipeng <song_zhipeng@foxmail.com>
2018-05-30 14:08:58 +0800

    Update wechat_warning.py

--- wechat_warning.py
+++ wechat_warning.py
@@ -1,19 +1,21 @@
 import requests
 from configparser import ConfigParser
 
-cfg=ConfigParser()
-cfg.read('config.ini')
+cfg = ConfigParser()
+cfg.read('.\config.ini')
 APPID = cfg.get('wechat_id','APPID')
 APP_SECRET = cfg.get('wechat_id','APP_SECRET')
 MY_WXID =cfg.get('wechat_id','MY_WXID')
 WX_MODEL_ID = cfg.get('wechat_id','WX_MODEL_ID')
 
-WX_MSG = {   "touser":MY_WXID,
+WX_MSG = {  
+     "touser":MY_WXID,
     "template_id":WX_MODEL_ID,
     "url":"http://www.google.com",
     "topcolor":"#FF000",
     "data":{
-        "type":{"value":"支付宝",# 可以修改成任意字符，对应模板中的{{type.DATA}}
+        "type":{
+                "value":"微信",# 对应模板中的{{type.DATA}}
                 }
     }
 }
@@ -25,8 +27,12 @@ def get_token():
     return(a.json()['access_token'])
 
 def send_msg(access_token):
-    msg_url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token="+access_token
+    msg_url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=" + access_token
     r = requests.post(msg_url,json=WX_MSG)
     print(r.content)
 
-send_msg(get_token())
+def get_informed():
+    send_msg(get_token())
+
+if __name__ == '__main__':
+    get_informed()
